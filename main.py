7b39171cb2cb247a7caca35c837e3a0bdd79f10d
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-04-30 14:48:17 +0300

    Receiving form data with WTForms.

--- main.py
+++ main.py
@@ -30,16 +30,6 @@ def login():
     return render_template('login.html', form=login_form)
 
 
-# @app.route('/success')
-# def success():
-#     return render_template('success.html')
-#
-#
-# @app.route('/denied')
-# def denied():
-#     return render_template('denied.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
 
