275d0b40f4552d0a2baa845c8901138d3588f51d
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-05-02 02:12:09 +0300

    Using flask bootstrap as an inherited template

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap4
 
 
 class LoginForm(FlaskForm):
@@ -11,6 +12,7 @@ class LoginForm(FlaskForm):
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap4(app)
 app.secret_key = "secretkey"
 
 
